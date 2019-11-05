455074f5d1ae8038df54ec9f3967e98200ce34fa
sushen <sushenbiswasaga@gmail.com>
2019-11-05 06:45:37 +0600

    Initial commit

---
+++ app/src/main/java/blog/video/biswas/earningtechnic/MainActivity.java
@@ -0,0 +1,44 @@
+package blog.video.biswas.earningtechnic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    public static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+            mFirebaseAuth = FirebaseAuth.getInstance();
+
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.PhoneBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()
+                            ))
+                            .setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
+    }
